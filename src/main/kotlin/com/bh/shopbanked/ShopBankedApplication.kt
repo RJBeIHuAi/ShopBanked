bef4741a52bc7477d932c66df8cfb45683d5745d
RJBeIHuAi <2841953564@qq.com>
2023-10-14 11:33:20 +0800

    Initial commit

---
+++ src/main/kotlin/com/bh/shopbanked/ShopBankedApplication.kt
@@ -0,0 +1,11 @@
+package com.bh.shopbanked
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class ShopBankedApplication
+
+fun main(args: Array<String>) {
+    runApplication<ShopBankedApplication>(*args)
+}
